1a270351b7a94a9032252fc78e34e6c3e022aab9
raiders032 <48238562+raiders032@users.noreply.github.com>
2019-06-15 23:01:01 +0900

    Update ML_learning.py

--- ML_learning.py
+++ ML_learning.py
@@ -2,35 +2,47 @@
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from keras.callbacks import EarlyStopping
 
-# setting random seed
+# Setting random seed
 np.random.seed(5)
 
-# 1. Load dataset
-dataset = np.loadtxt("./data.csv", delimiter=",")
+# Load dataset
+dataset = np.loadtxt("./forML_V2.csv", delimiter=",")
+x_train = dataset[:2660,:200]
+y_train = dataset[:2660,200]
+x_test = dataset[2660:,:200]
+y_test = dataset[2660:,200]
 
-# 2. Reshaping the data
-x_train = dataset[:700,0:8]
-y_train = dataset[:700,8]
-x_test = dataset[700:,0:8]
-y_test = dataset[700:,8]
+x_train /=1700
+x_test /=1700
 
-# 3. Creating the DNN model and Adding layers to the model
+# Creating the DNN model and Adding layers to the model
 model = Sequential()
-model.add(Dense(12, input_dim=8, activation='relu'))
-model.add(Dense(8, activation='relu'))
+model.add(Dense(200, input_dim=200, activation='relu'))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(25, activation='relu'))
+model.add(Dense(10, activation='relu'))
+model.add(Dense(5, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
-# 4. Compiling the model
+# Compiling the model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# 5. Training the model
-model.fit(x_train, y_train, epochs=3000, batch_size=128)
+# Training the model
+#early_stopping = EarlyStopping(patience = 20)
+model.fit(x_train, y_train, epochs=100, batch_size=128, verbose=1,validation_data=(x_test, y_test))#,callbacks=[early_stopping])
 
-# 6. Evaulating the model
-scores = model.evaluate(x_test, y_test)
-print("%s: %.2f%%" %(model.metrics_names[1], scores[1]*100))
+# Evaulating the model
+eval_model=model.evaluate(x_test, y_test)
+print(eval_model)
 
-#7.Saving the model
+# Prediction
+y_pred = model.predict(x_test)
+
+# Saving the model
 from keras.models import load_model
-model.save('ML.h5')
+model.save('ML_V2.h5')
