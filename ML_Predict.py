906ece38f21fb4d4a480775f1172132f2f4edd95
raiders032 <48238562+raiders032@users.noreply.github.com>
2019-06-17 02:50:29 +0900

    Update ML_Predict.py

--- ML_Predict.py
+++ ML_Predict.py
@@ -8,54 +8,48 @@ import pymysql
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from keras.callbacks import EarlyStopping 
+import time
 
-# MySQL Connection
+# MySQL Connection 연결
 conn = pymysql.connect(host='localhost', user='admin', password='4885',
                        db='cafe', charset='utf8')
-# Create Dictoionary Cursor 
+# Connection 으로부터 Dictoionary Cursor 생성
 curs = conn.cursor(pymysql.cursors.DictCursor) 
 
-# Load dataset
-dataset = np.loadtxt("./data2.csv", delimiter=",")
-end_idx=(len(dataset))
-pre_date = int(dataset[end_idx-1][1])
+# Loading the model
+from keras.models import load_model
+model = load_model('ML_V2.h5')
+
+pre_flag_list=[3,3,3,3]
 
 while(True):
+  time.sleep(3);
   # Load dataset
   dataset = np.loadtxt("./data2.csv", delimiter=",")
   end_idx=(len(dataset))
   cur_date = int(dataset[end_idx-1][1])
-  if cur_date == pre_date :
-    continue
-  else:
-    pre_date=cur_date
-  #standardizing the input feature
-  X = dataset[end_idx-4:end_idx,2:202]
-  X /=1700
-  Y = dataset[end_idx-4:end_idx,202]
   
-  # Loading the model
-  from keras.models import load_model
-  model = load_model('ML.h5')
+  #standardizing the input feature
+  X = dataset[end_idx-4:end_idx,2:102]
+  X /=1700
+  Y = dataset[end_idx-4:end_idx,202] 
 
   # prediction
-  yhat = model.predict(X)
-  yhat2 = model.predict_classes(X)
+  yhat = model.predict_classes(X)
   flag={'101101101':2,'101101102':2,'101102101':2,'101102102':2}
   for i in range (1,5):
     if flag[str(int(dataset[end_idx-i][0]))]==2:
-      print(str(int(dataset[end_idx-i][0]))+"is chaged")
-      flag[str(int(dataset[end_idx-i][0]))]=int(yhat2[4-i])
+      flag[str(int(dataset[end_idx-i][0]))]=int(yhat[4-i])
     else:
       break
+  if pre_flag_list==list(flag.values()):
+    continue
+  pre_flag_list = list(flag.values())
   print(cur_date)	  
-  print(yhat)
-  print(yhat2)  
   print(flag)
   sql = """
   insert into result(C01T01S01,C01T01S02,C01T02S01,C01T02S02)
   	     values (%s, %s, %s, %s)"""
   curs.execute(sql, (flag['101101101'],flag['101101102'],flag['101102101'],flag['101102102']))
   conn.commit()
- 
 conn.close()	
