3ebcb97403dca021ea260b4152d52620598a1f77
raiders032 <48238562+raiders032@users.noreply.github.com>
2019-06-17 00:10:25 +0900

    Update ML_learning.py

--- ML_learning.py
+++ ML_learning.py
@@ -1,7 +1,7 @@
 # Import modules
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from keras.callbacks import EarlyStopping
@@ -11,21 +11,20 @@ np.random.seed(5)
 
 # Load dataset
 dataset = np.loadtxt("./forML_V2.csv", delimiter=",")
-x_train = dataset[:2660,:200]
-y_train = dataset[:2660,200]
-x_test = dataset[2660:,:200]
-y_test = dataset[2660:,200]
+x_train = dataset[:4000,:100]
+y_train = dataset[:4000,200]
+x_test = dataset[4000:,:100]
+y_test = dataset[4000:,200]
 
 x_train /=1700
 x_test /=1700
 
 # Creating the DNN model and Adding layers to the model
 model = Sequential()
-model.add(Dense(200, input_dim=200, activation='relu'))
+model.add(Dense(100, input_dim=100, activation='relu'))
 model.add(Dense(100, activation='relu'))
 model.add(Dense(50, activation='relu'))
 model.add(Dense(25, activation='relu'))
-model.add(Dense(10, activation='relu'))
 model.add(Dense(5, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
@@ -34,7 +33,7 @@ model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']
 
 # Training the model
 #early_stopping = EarlyStopping(patience = 20)
-model.fit(x_train, y_train, epochs=100, batch_size=128, verbose=1,validation_data=(x_test, y_test))#,callbacks=[early_stopping])
+model.fit(x_train, y_train, epochs=100, batch_size=64, verbose=1,validation_data=(x_test, y_test))#,callbacks=[early_stopping])
 
 # Evaulating the model
 eval_model=model.evaluate(x_test, y_test)
