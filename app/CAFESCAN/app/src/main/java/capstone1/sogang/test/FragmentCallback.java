62603c639ac6b32cfbdc7ee80b3f8bf3415ba693
buyeolim <buyeolim@gmail.com>
2019-06-17 04:07:36 +0900

    앱 수정

---
+++ app/CAFESCAN/app/src/main/java/capstone1/sogang/test/FragmentCallback.java
@@ -0,0 +1,9 @@
+package capstone1.sogang.test;
+
+import android.os.Bundle;
+
+public interface FragmentCallback {
+
+    public void onFragmentSelected(int position, Bundle bundle);
+
+}
---
+++ app/CAFESCAN/app/src/main/java/capstone1/sogang/test/Map.java
@@ -0,0 +1,46 @@
+package capstone1.sogang.test;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
+
+import net.daum.mf.map.api.MapView;
+
+public class Map extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.map, container, false);
+
+        //지도
+        MapView mapView = new MapView(getActivity());
+        ViewGroup mapViewContainer = (ViewGroup) rootView.findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
+        // 중심점 변경
+        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.550017, 126.942000), true);
+
+        // 줌 레벨 변경
+        mapView.setZoomLevel(4, true);
+
+        //마커 찍기
+        MapPoint MARKER_POINT = MapPoint.mapPointWithGeoCoord(37.550017, 126.9420004);
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName("아담샬관");
+        marker.setTag(0);
+        marker.setMapPoint(MARKER_POINT);
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+
+        mapView.addPOIItem(marker);
+
+        return rootView;
+    }
+}
